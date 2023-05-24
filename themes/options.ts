910f401941a00db0b6a100ad36d4b230183b49a2
Oscar <o.velilla@gmail.com>
2023-05-24 11:56:17 +0200

    Modified Options

--- themes/options.ts
+++ themes/options.ts
@@ -2,4 +2,11 @@ export const options = {
   typography: {
     fontFamily: "Poppins, sans-serif",
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {},
+      },
+    },
+  },
 };
